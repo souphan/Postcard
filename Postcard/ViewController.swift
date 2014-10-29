78f7db76ebcefa06e017f6c9782ad99d3dc79229
System Administrator <root@souphans-MacBook.local>
2014-10-28 20:06:59 -0500

    nameLabel functionality

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
     
+    @IBOutlet weak var nameLabel: UILabel!
     
     @IBOutlet weak var enterNameTextField: UITextField!
     
@@ -23,7 +24,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -35,6 +36,13 @@ class ViewController: UIViewController {
         messageLabel.hidden = false
         messageLabel.text = enterAMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
         
         enterAMessageTextField.text = ""
         enterAMessageTextField.resignFirstResponder()
